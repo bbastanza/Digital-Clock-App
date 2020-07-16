f4b32ede3bdf3d6dd4d7c92dc9209d35224f6b61
bbastanza <b.bastanza@gmail.com>
2020-07-16 14:00:00 -0600

    all but button

--- index.js
+++ index.js
@@ -1,56 +1,85 @@
-// examine the document object
+// main function
+function digiClock() {
 
-// console.log(document) to find info about a current HTML
-// gets info on todays date and time
-let today = new Date();
+    // let twentyFour = false;
 
-// gets info on todays date and assigns it to the variable "date"
-let date = (today.getMonth() + 1) + "|" + today.getDate() + "|" + (today.getFullYear() - 2000);
 
-// gets info on todays time and assigns it to the variable "time"----> this does not tick
-// let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    // THIS IS WHERE I AM TYING TO ADD THE BUTTON FUNCTIONALITY
+    // IF YOU COMMENT OUT LINES 10-21 THE CLOCK WILL RUN IN 12 HOUR TIME AS EXPECTED
+    // LINE 39 IS WHERE THE CHANGE SHOULD HAPPEN
+    function switchTwentyfour(twentyFour) {
+        if (twentyFour == false) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
 
-function hourCount() {
+    let twentyFour = false;
+
+    let button = document.getElementById("switch");
+    button.onclick = switchTwentyfour(twentyFour);
+    twentyFour = switchTwentyfour();
+
+
+    // grabs the date and time info from the sky :)
     let today = new Date();
-    let hour = setInterval(today.getHours(), 1000);
-    h = String(hour);
-    return h;
+
+    // sets variable to for all necessary time/date functions
+    let sec = today.getSeconds();
+    let min = today.getMinutes();
+    let hour = today.getHours();
+    let mon = (today.getMonth() + 1);
+    let day = today.getDate();
+    let year = (today.getFullYear() - 2000);
+
+    // makes it 12 hour time
+
+
+    /// THIS IS WHERE I WAS TRYING TO ADD BUTTON FUNCTIONALITY ///
+    if (twentyFour == false) {
+        if (hour > 12) {
+            hour = hour - 12;
+        }
+    }
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+    // sets midnight to 12
+    if (hour == 0) {
+        hour = 12;
+    }
+
+    // 2 digit formats
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+    if (min < 10) {
+        min = "0" + min;
+    }
+    if (hour < 10) {
+        hour = "0" + hour;
+    }
+    if (day < 10) {
+        day = "0" + day;
+    }
+    if (mon < 10) {
+        mon = "0" + mon;
+    }
+
+    // formats for upload
+    let date = mon + "|" + day + "|" + year;
+    let time = hour + ":" + min + ":" + sec;
+
+    // changes the HTML elements by ID to the above date and time 
+    let datevar = document.getElementById("date");
+    datevar.textContent = date;
+    let timevar = document.getElementById("clock");
+    timevar.textContent = time;
+
 }
 
-function minCount() {
-    let today = new Date();
-    let min = setInterval(today.getMinutes(), 1000);
-    m = String(min);
-    return m;
-}
-
-function secCount() {
-    let today = new Date();
-    let sec = setInterval(today.getSeconds(), 1000);
-    s = String(sec);
-    return s;
-}
-
-
-console.log(secCount())
-
-let time = hourCount() + ':' + minCount() + ':' + secCount();
-
-
-
-// logs the date in console
-console.log(date);
-
-// logs the time in console
-console.log(time);
-
-
-
-// get element by id //
-let datevar = document.getElementById("date")
-datevar.textContent = date
-
-let timevar = document.getElementById("clock")
-timevar.textContent = time
+// intervals the clock
+setInterval(digiClock, 1000);
 
 
