c532f4e42b6758a320f8981e2bc0f3dc195d73a6
bbastanza <b.bastanza@gmail.com>
2020-07-17 15:15:18 -0700

    next commit

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-    setInterval(update, 1000)
+    digiClock();
 });
 
-
+/// I MOVED THIS VARIABLE UP HERE BECAUSE THERE WAS A VALUE ERROR
 let twentyFour = true;
+
+/// THIS IS THOUGHT WOULD CALL THE CLICK... THERE IS A NEW UNCAUGHT TYPE ERROR
+///>>>>>Uncaught TypeError: buttonPress is null<<<<<<< LINE 12
+let buttonPress = document.getElementById("switch")
+
+buttonPress.addEventListener("click", function () {
+    switchTwentyfour(twentyFour);
+
+}
+)
 
 function update() {
     let today = new Date();
@@ -26,91 +36,62 @@ function update() {
         mon = "0" + mon;
     }
 
-    // THIS IS WHERE I TRIED TO IMPLIMENT THE twentyFour FUNCTION
-    let x = switchTwentyfour(twentyFour)
-    if (x = false)
-        hour = hour - 12;
-
-
-    if (hour.toString().length < 2) {
-        hour = "0" + hour;
-    }
     // sets midnight to 12
     if (hour == 0) {
         hour = 12;
     }
 
 
+    /// HERE IS WHERE I CALLED THE FORMAT FUNCTION ///
+    formatTime(sec, min, hour);
 
 
     let date = mon + "|" + day + "|" + year;
-    let time = hour + ":" + min + ":" + sec;
-
-
     let datevar = document.getElementById("date");
     datevar.textContent = date;
-    let timevar = document.getElementById("clock");
-    timevar.textContent = time;
+    // let timevar = document.getElementById("clock");
+    // timevar.textContent = time;
 }
 
-// THIS IS WHERE I TRIED TO IMPLIMENT THE BUTTON AGAIN
-// IT STILL ISN'T FUNCTIONING PROPERLY BECAUSE WHEN I CHANGE THE VALUE OF 
-// twentyFour  ^^^^ AT THE TOP ^^^ FROM TRUE TO FALSE IT IS STILL VIEWED IN 24 HOUR TIME
+/// THIS IS THE NEW FORMATING FUNCTION. I THOUGHT IT WOULD BE BETTER TO 
+/// JUST INCLUDE THE TIME BECAUSE IT IS THE ONLY THING THAT HAS THE CHANGE
+function formatTime(sec, min, hour) {
+    if (twentyFour == false) {
+        if (hour.toString().length < 2) {
+            hour = "0" + hour;
+        }
+        time = (hour - 12) + ":" + min + ":" + sec;
+        let timevar = document.getElementById("clock");
+        timevar.textContent = time;
+    } else {
+        if (hour.toString().length < 2) {
+            hour = "0" + hour;
+        }
+        time = hour + ":" + min + ":" + sec;
+        let timevar = document.getElementById("clock");
+        timevar.textContent = time;
+    }
+}
 
-
+// *** I ADDED THE CONSOLE LOG TO SEE IF THE BUTTON WAS TRIGGARING THIS FUNCTION AT ALL... NO DICE ***
 // function to switch from 24/12 hour time
 function switchTwentyfour(twentyFour) {
+    console.log("HI")
     if (twentyFour == false) {
         twentyFour = true;
-        return twentyFour;
     }
     if (twentyFour == true) {
-        twentyFour == false;
-        return twentyFour;
+        twentyFour = false;
     }
-    // on button click this triggers the switchTwentyfour()^^^^
-    let button = document.getElementById("switch");
-    button.onclick = switchTwentyfour(twentyFour);
-    twentyFour = switchTwentyfour(twentyFour);
 }
-
-// I HAD A THOUGHT TO CLEAN THE CODE UP BY PUTTING THE FORMATTING IN THIS FUNCTION
-// I COULDN'T GET IT TO FUNCTION PROPERLY THOUGH SO ITS COMMENTED OUT AND
-// THIS CODE IS BACK IN THE UPDATE FUNCTION
-
-// function formats() {
-//     if (sec.length < 2) {
-//         sec = "0" + sec;
-//     }
-//     if (min.length < 2) {
-//         min = "0" + min;
-//     }
-//     if (hour.length < 2) {
-//         hour = "0" + hour;
-//     }
-//     if (day.length < 2) {
-//         day = "0" + day;
-//     }
-//     if (mon.length < 2) {
-//         mon = "0" + mon;
-//     }
-//     // sets midnight to 12
-//     if (hour == 0) {
-//         hour = 12;
-//     }
+// on button click this triggers the switchTwentyfour()^^^^
+//     let button = document.getElementById("switch");
+//     button.onclick = switchTwentyfour(twentyFour);
+//     twentyFour = switchTwentyfour(twentyFour);
 // }
 
-// I TRIED HERE TO SIMPLIFY THE MAIN FUNCTION, BUT IT KEPT BREAKING THE BROWSER WHEN
-// IT SAW THIS WHILE LOOP (I THINK)
-// main function
 function digiClock() {
-
-
-    while (true) {
-        setInterval(update, 1000);
-
-
-    }
+    setInterval(update, 1000);
 }
 
 
